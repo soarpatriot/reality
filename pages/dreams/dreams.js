74695774ee337778103fefd496bd655292d2db26
Soar <85624529@qq.com>
2016-11-23 00:46:38 +0800

    first dream

--- pages/dreams/dreams.js
+++ pages/dreams/dreams.js
@@ -1,3 +1,34 @@
 Page({
-  
+  data: {
+    dreams: []
+  },
+
+  //view加载
+  onLoad: function() {
+    console.log( 'onLoad' )
+    var that = this
+
+    //网络请求
+    wx.request( {
+      url: 'https://api.dreamreality.cn/posts',
+      header: {
+        "Content-Type": "application/json"
+      },
+      method: "GET",
+      
+      success: function( res ) {
+        //获取到了数据
+        console.log("success")
+        var dreams = res.data.data;
+        console.log( dreams );
+        that.setData( {
+          dreams: dreams
+        })
+        //that.update()
+      },
+      fail: function(error){
+          console.log(error)
+      }
+    });
+  }
 })
