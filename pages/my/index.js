6df5166a648406490889ef1b2878b92b43378204
Soar <85624529@qq.com>
2016-12-22 00:41:48 +0800

    doing the my tab

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,8 +1,36 @@
 // pages/my/index.js
+var app = getApp()
 Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    var that = this
+    // 页面初始化 options为页面跳转所带来的参数
+       
+    //网络请求
+    var userId = app.globalData.userInfo.id
+    var oneUrl = "https://api.dreamreality.cn/users/"+userId
+    wx.request( {
+      url: oneUrl,
+      header: {
+        "Content-Type": "application/json"
+      },
+      method: "GET",
+      
+      success: function( res ) {
+        //获取到了数据
+        console.log("success")
+        var user = res.data.data;
+        console.log( user );
+        that.setData( {
+          user: user
+        })
+        //that.update()
+      },
+      fail: function(error){
+          console.log(error)
+      }
+    });
   },
   onReady:function(){
     // 页面渲染完成
