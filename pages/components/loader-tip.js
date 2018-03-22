7647cfb80f2913c442d8b56336487f0d93a45082
Soar <85624529@qq.com>
2018-03-22 19:11:57 +0800

    change the load more style

---
+++ pages/components/loader-tip.js
@@ -0,0 +1,26 @@
+// pages/components/loader-tip.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    hide: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
